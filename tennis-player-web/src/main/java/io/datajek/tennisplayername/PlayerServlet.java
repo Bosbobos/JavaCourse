564645dfbd839a061f602de4b8beed9ae4683811
Матвей Коновалов <ok__@outlook.com>
2024-03-29 20:31:53 +0300

    Created MVC web application with jsp view file

---
+++ tennis-player-web/src/main/java/io/datajek/tennisplayername/PlayerServlet.java
@@ -0,0 +1,18 @@
+package io.datajek.tennisplayername;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/player.do")
+public class PlayerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(request, response);
+    }
+}
---
+++ tennis-player-web/src/main/webapp/WEB-INF/views/welcome.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Matvey Konovalov
+  Date: 29.03.2024
+  Time: 20:17
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Tennis Player DB</title>
+</head>
+<body>
+<h2>Welcome to the tennis player database!</h2>
+</body>
+</html>
