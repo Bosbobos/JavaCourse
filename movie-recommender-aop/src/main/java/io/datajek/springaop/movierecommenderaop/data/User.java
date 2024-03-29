7a1d767604c2359b40f65f1c12ab5315610a570d
Матвей Коновалов <ok__@outlook.com>
2024-03-29 05:07:26 +0300

    Implemented starter AOP logic with @Before annotation

---
+++ movie-recommender-aop/src/main/java/io/datajek/springaop/movierecommenderaop/data/User.java
@@ -0,0 +1,10 @@
+package io.datajek.springaop.movierecommenderaop.data;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class User {
+    public String getUserDetails() {
+        return "user details";
+    }
+}
